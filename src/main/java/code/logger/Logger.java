2a11858533904e38e77c5b2e12c471f0a03b1633
Anas Elgarhy <anas.elgarhy.dev@gmail.com>
2021-12-22 01:33:39 +0200

    Add the logger abstract class

---
+++ src/main/java/code/logger/Logger.java
@@ -0,0 +1,42 @@
+package code.logger;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+public abstract class Logger implements Runnable {
+    protected ArrayList<Key> keys; // list of keys pressed
+    protected Map<Integer, Integer> keyBuffer; // map of key codes to key codes temporarily
+
+
+    public Logger() {
+        keys = new ArrayList<>(); // initialize keys array list
+        keyBuffer = new HashMap<>(); // initialize key buffer hash map
+    }
+
+    // Add a key to the list of keys pressed method
+    public void addKey(Key key) {
+        keys.add(key);
+    }
+
+    // Remove a key from the list of keys pressed method
+    public void removeKey(Key key) {
+        keys.remove(key);
+    }
+
+    // Get the specific key from the list of keys pressed method
+    public Key getKey(int index) {
+        return keys.get(index);
+    }
+
+    // Get current index
+    public int getIndex() {
+        return keys.size() - 1;
+    }
+
+    // Start logging method
+    public abstract void start();
+
+    // Stop logging method
+    public abstract void stop();
+}
